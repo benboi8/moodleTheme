af41a47a2fd3d7e5cf398dfd76380ffb67b1e5c4
benboi8 <benboi294@gmail.com>
2024-06-07 18:38:08 +0100

    theme changes

--- settings.php
+++ settings.php
@@ -15,39 +15,6 @@ if ($ADMIN->fulltree) {
     // Each page is a tab - the first is the "General" tab.
     $page = new admin_settingpage('theme_ben_general', get_string('generalsettings', 'theme_ben'));
 
-    // Replicate the preset setting from boost.
-    $name = 'theme_ben/preset';
-    $title = get_string('preset', 'theme_ben');
-    $description = get_string('preset_desc', 'theme_ben');
-    $default = 'default.scss';
-
-    // We list files in our own file area to add to the drop down. We will provide our own function to
-    // load all the presets from the correct paths.
-    $context = context_system::instance();
-    $fs = get_file_storage();
-    $files = $fs->get_area_files($context->id, 'theme_ben', 'preset', 0, 'itemid, filepath, filename', false);
-
-    $choices = [];
-    foreach ($files as $file) {
-        $choices[$file->get_filename()] = $file->get_filename();
-    }
-    // These are the built in presets from Boost.
-    $choices['default.scss'] = 'default.scss';
-    $choices['plain.scss'] = 'plain.scss';
-
-    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
-    $setting->set_updatedcallback('theme_reset_all_caches');
-    $page->add($setting);
-
-    // Preset files setting.
-    $name = 'theme_ben/presetfiles';
-    $title = get_string('presetfiles','theme_ben');
-    $description = get_string('presetfiles_desc', 'theme_ben');
-
-    $setting = new admin_setting_configstoredfile($name, $title, $description, 'preset', 0,
-        array('maxfiles' => 20, 'accepted_types' => array('.scss')));
-    $page->add($setting);
-
     // Variable $brand-color.
     // We use an empty default value because the default colour should come from the preset.
     $name = 'theme_ben/brandcolor';
